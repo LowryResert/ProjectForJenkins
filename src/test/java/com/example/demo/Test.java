6344cad42a66ca04fcae8da67ba24c3824f67b03
ruiqingluo <ruiqingluo1@163.com>
2021-07-07 20:14:07 +0800

    # init

---
+++ src/test/java/com/example/demo/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[][] a = new int[3][4];
+
+        System.out.println(Arrays.deepToString(a));
+    }
+}
