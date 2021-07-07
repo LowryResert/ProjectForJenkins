c794542945f671cb813cd06629a48d6906559b82
ruiqingluo <ruiqingluo1@163.com>
2021-07-07 20:15:05 +0800

    # fixed

--- src/test/java/com/example/demo/Test.java
+++ src/test/java/com/example/demo/Test.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        int[][] a = new int[3][4];
-
-        System.out.println(Arrays.deepToString(a));
+        System.out.println("Hello Jenkins");
     }
 }
